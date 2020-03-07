ac65fb7ffe5070fd8023c0979e3ed7bb2da52a8e
W-Borkowski <xboreek99@gmail.com>
2020-03-07 11:06:50 +0100

    first file

---
+++ wsb/PAI/struktura.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pl" dir="ltr"
+
+
+
+<head>
+<meta charset="utf-8">
+<title> Struktura dokumentu </title>
+</head>
+
+<body>
+<?php
+echo "test";
+
+
+</body>
+
+
+
+
+
+
+
+</html>
